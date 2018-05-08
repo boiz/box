21896680ebf0279e1bca4ca7ee435e356bb262b8
boiz <hairypriest@hotmail.com>
2018-05-07 19:42:05 -0700

    create new masterpiece haha

--- 10.js
+++ 10.js
@@ -1,8 +1,32 @@
 const fs=require("fs");
 
 
-fs.watch("d:/",err=>{
-	console.log(err);
+/*fs.watch("c:/",{recursive:true},(path,filename)=>{
+	console.log(path,filename);
 });
 
 
+*/
+
+/*
+fs.readdir("c:/users/administrator/box/inbox",(a,b)=>{
+	console.log(a,b);
+});
+
+*/
+
+
+
+
+/*fs.watch("c:/Users/administrator/box",{recursive:true},(a,b)=>{
+	console.log(a,b);
+});*/
+
+
+
+
+/*
+
+fs.watch("c:/users/administrator/box/inbox",{recursive:true},(a,b)=>{
+	console.log(a,b);
+});*/
--- 7.js
+++ 7.js
@@ -1,18 +1,44 @@
 const fs=require("fs");
 const path=require("path");
 
+let inbox="C:/Users/administrator/Box/testinbox";
+let data="z:/app/stores";
+
+
 let trigChange=pathname=>{
   fs.mkdir(pathname,err=>{
     fs.rmdir(pathname,err=>{});
   });
 }
 
-fs.watch("inbox",(event, storeFolder)=>{
+let renamePlus=(original,destination,callback)=>{
+  fs.copyFile(original,destination,err=>{
+    fs.unlink(original,callback);
+  });
+}
 
-  let storePath=`inbox/${storeFolder}`;
+let processing=[];
 
-	fs.readdir(storePath,(err,files)=>{
-    console.log(files.length);
+let deleteElement=(array,element)=>{
+  let index = array.indexOf(element);
+  if (index > -1) array.splice(index, 1);
+  return array;
+}
+
+
+fs.watch(inbox,{recursive:true},(event, storeFolder)=>{
+
+  storeFolder=path.dirname(storeFolder);
+  
+  //console.log(event,storeFolder);
+
+  let storePath=`${inbox}/${storeFolder}`;
+
+	
+  fs.readdir(storePath,(err,files)=>{
+
+
+    //console.log(files.length);
     if(files.length>0){
 
       for(let x of files){
@@ -35,24 +61,37 @@ fs.watch("inbox",(event, storeFolder)=>{
 
         let targetFolder;
 
-        targetFolder=`data/${categoryFolder}/${name}`;
+        targetFolder=`${data}/${categoryFolder}/${name}`;
 
 
         fs.mkdir(targetFolder,err=>{
         
           targetFilename=`${filename}_${storeFolder}_${(new Date).getTime()}${ext}`;
 
-          fs.rename(originalPath,`${targetFolder}/${targetFilename}`,err=>{
+/*
+          console.log({
+            from:originalPath,
+            to:`${targetFolder}/${targetFilename}`
+          });*/
+
+          //console.log(processing.length);
+
+          if(processing.includes(originalPath)) return;
+
+          renamePlus(originalPath,`${targetFolder}/${targetFilename}`,err=>{
+
+            console.log(`start copying ${originalPath}`);
             if(err){
-              console.log(err);
+              //console.log(err);
               /*change event trigger*/
-              setTimeout(()=>{trigChange(tempPath)},500);
+              //setTimeout(()=>{trigChange(tempPath)},500);
             }
-            else console.log({
-              from:originalPath,
-              to:`${targetFolder}/${targetFilename}`
-            });
+            else deleteElement(processing,originalPath);
           });
+
+          if(!processing.includes(originalPath)) processing.push(originalPath);
+
+
 
         });
       }
